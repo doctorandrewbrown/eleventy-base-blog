b948afea7d673a32cc2c10f722b8b9c8857c2728
andrew brown <dr.andrew.david.brown@gmail.com>
2024-12-10 17:58:27 +0000

    change meta data and add test post

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
+	title: "Andrew David Brown",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "A Bridgend web developer's coding blog",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Andrew Brown",
+		email: "dr.andrew.david.brown@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
--- content/blog/test-post.md
+++ content/blog/test-post.md
@@ -1,7 +1,7 @@
 ---
 title: A test post.
 description: This is a post on My Blog about win-win survival strategies.
-date: 2024-10-24
+date: 2024-11-10
 tags: ["test"]
 ---
 # Code Test
